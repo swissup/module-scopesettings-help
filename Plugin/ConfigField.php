3b07ba821bef609b3ac78c63fca6594cbe6aa324
Vitaliy Ohnitskiy <tmvitaly@gmail.com>
2023-05-31 15:46:28 +0300

    Show all config settings in the tooltip. (Not filtering data yet.)

--- Plugin/ConfigField.php
+++ Plugin/ConfigField.php
@@ -3,11 +3,16 @@
 namespace Swissup\ScopesettingsHelp\Plugin;
 
 use Magento\Config\Model\Config\Structure\Element\Field;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Api\Data\WebsiteInterface;
+use Magento\Store\Model\ScopeInterface;
 use Magento\Store\Model\Store;
 
 class ConfigField
 {
+    const SCOPE_TYPE_WEBSITE = 'website';
+    const SCOPE_TYPE_STORE = 'store';
+
     /**
      * @var \Swissup\ScopesettingsHelp\Helper\Data
      */
@@ -29,18 +34,25 @@ class ConfigField
     private $storeRepository;
 
     /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
      * @param \Swissup\ScopesettingsHelp\Helper\Data $helper
      */
     public function __construct(
         \Swissup\ScopesettingsHelp\Helper\Data $helper,
         \Magento\Framework\App\RequestInterface $request,
         \Magento\Store\Api\WebsiteRepositoryInterface $websiteRepository,
-        \Magento\Store\Api\StoreRepositoryInterface $storeRepository
+        \Magento\Store\Api\StoreRepositoryInterface $storeRepository,
+        ScopeConfigInterface $scopeConfig
     ) {
         $this->helper = $helper;
         $this->request = $request;
         $this->websiteRepository = $websiteRepository;
         $this->storeRepository = $storeRepository;
+        $this->scopeConfig = $scopeConfig;
     }
 
     /**
@@ -59,9 +71,12 @@ class ConfigField
         $mergeArrs = array_merge($websitesArray, $storesArray);
 
         /* get unique values and convert to string */
-        $tooltipContent;
+        $tooltipContent = '';
         foreach ($mergeArrs as $item) {
-            $tooltipContent = implode("<br />", array_unique($item));
+            if (empty($item)) {
+                continue;
+            }
+            $tooltipContent .= implode(" ", array_unique($item)) . "<br />";
         }
 
         return $tooltipContent;
@@ -74,9 +89,10 @@ class ConfigField
     private function getWebsiteList($subject): array
     {
         $websiteList = [];
+        $scopeType = $this->getScopeTypeWebsite();
         foreach ($this->websiteRepository->getList() as $website) {
             if ($website->getCode() === WebsiteInterface::ADMIN_CODE) {
-                $websiteScopeInfo = $this->getScopeSettingsInfo($subject, $website);
+                $websiteScopeInfo = $this->getScopeSettingsInfo($subject, $scopeType, $website);
                 $websiteList[] = $websiteScopeInfo;
             }
         }
@@ -91,11 +107,11 @@ class ConfigField
     private function getStoreList($subject): array
     {
         $storeList = [];
+        $scopeType = $this->getScopeTypeStore();
         foreach ($this->storeRepository->getList() as $store) {
-            if ($store->getCode() === Store::ADMIN_CODE) {
-                $storeScopeInfo = $this->getScopeSettingsInfo($subject, $store);
-                $storeList[] = $storeScopeInfo;
-            }
+            /* return settings info for each store */
+            $storeScopeInfo = $this->getScopeSettingsInfo($subject, $scopeType, $store);
+            $storeList[] = $storeScopeInfo;
         }
 
         return $storeList;
@@ -103,14 +119,43 @@ class ConfigField
 
     /**
      *  @param $subject, $scope
-     *  @return
+     *  @return array
      */
-    private function getScopeSettingsInfo($subject, $scope)
+    private function getScopeSettingsInfo($subject, $scopeType, $scope): array
     {
+        $configPath = $subject->getPath();
         $scopeInfo = [];
-        $scopeInfo[] = 'here will be scope info-1';
-        $scopeInfo[] = 'here will be scope info-2';
-        $scopeInfo[] = 'here will be scope info-1';
+
+        if ($scopeType === self::SCOPE_TYPE_WEBSITE) {
+            $configValue = $this->scopeConfig->getValue(
+                $configPath,
+                ScopeInterface::SCOPE_WEBSITE,
+                $scope
+            );
+            $scopeCode = $scope->getCode();
+        } else {
+            $configValue = $this->scopeConfig->getValue(
+                $configPath,
+                ScopeInterface::SCOPE_STORE,
+                $scope->getId()
+            );
+            $scopeCode = $scope->getCode();
+        }
+
+        if (!is_null($configValue)) {
+            $scopeInfo[] = $configValue . " - " . $scopeCode;
+        }
+
         return $scopeInfo;
+    }
+
+    private function getScopeTypeWebsite(): string
+    {
+        return self::SCOPE_TYPE_WEBSITE;
+    }
+
+    private function getScopeTypeStore(): string
+    {
+        return self::SCOPE_TYPE_STORE;
     }
 }
